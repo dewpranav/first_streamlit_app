e7455c5fa64bee2feba887017cfa6cb655c240e1
dewpranav <125552938+dewpranav@users.noreply.github.com>
2023-02-16 15:26:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 & bluberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard boiled free range Egg')

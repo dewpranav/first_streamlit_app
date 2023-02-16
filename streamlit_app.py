d6ac597fd43ca15efca586962ead933d987924e0
dewpranav <125552938+dewpranav@users.noreply.github.com>
2023-02-16 15:28:50 +0530

    Update streamlit_app.py
    added images

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & bluberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled free range Egg')
+streamlit.text('🥣Omega 3 & bluberry oatmeal')
+streamlit.text('🥗🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🐔Hard boiled free range Egg')
+ 

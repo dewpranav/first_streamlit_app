ed57b9ed9d36a5b8fb7d5477655409b07bd5adb6
dewpranav <125552938+dewpranav@users.noreply.github.com>
2023-02-16 15:25:05 +0530

    Update streamlit_app.py
    added items for menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & bluberry oatmeal')
+streamlit.text('Hard boiled free range Egg')
